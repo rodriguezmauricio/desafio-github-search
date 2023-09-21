d138f59f24d5f8d9b941ddd12fef3fb758cc194e
rodriguezmauricio <contato.mauricioalves@gmail.com>
2023-09-20 21:21:37 -0300

    parte 1 funcionando do app

--- app/src/main/java/br/com/igorbag/githubsearch/ui/MainActivity.kt
+++ app/src/main/java/br/com/igorbag/githubsearch/ui/MainActivity.kt
@@ -1,15 +1,25 @@
 package br.com.igorbag.githubsearch.ui
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import br.com.igorbag.githubsearch.R
 import br.com.igorbag.githubsearch.data.GitHubService
 import br.com.igorbag.githubsearch.domain.Repository
+import br.com.igorbag.githubsearch.ui.adapter.RepositoryAdapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,11 +27,13 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnConfirmar: Button
     lateinit var listaRepositories: RecyclerView
     lateinit var githubApi: GitHubService
+    var retrofit: Retrofit? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupView()
+        setupListeners()
         showUserName()
         setupRetrofit()
         getAllReposByUserName()
@@ -30,21 +42,48 @@ class MainActivity : AppCompatActivity() {
     // Metodo responsavel por realizar o setup da view e recuperar os Ids do layout
     fun setupView() {
         //@TODO 1 - Recuperar os Id's da tela para a Activity com o findViewById
+        nomeUsuario = findViewById(R.id.et_nome_usuario)
+        btnConfirmar = findViewById(R.id.btn_confirmar)
+        listaRepositories = findViewById(R.id.rv_lista_repositories)
     }
 
     //metodo responsavel por configurar os listeners click da tela
     private fun setupListeners() {
         //@TODO 2 - colocar a acao de click do botao confirmar
+        btnConfirmar.setOnClickListener {
+            saveUserLocal()
+        }
     }
 
 
     // salvar o usuario preenchido no EditText utilizando uma SharedPreferences
     private fun saveUserLocal() {
         //@TODO 3 - Persistir o usuario preenchido na editText com a SharedPref no listener do botao salvar
+        val sharedPreferences = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
+        val textoInserido = nomeUsuario.text.toString()
+
+        val editor = sharedPreferences.edit()
+        editor.putString("userKey", textoInserido)
+        editor.apply()
+
+        Toast.makeText(this, "Usuário $textoInserido salvo com sucesso!", Toast.LENGTH_SHORT).show()
     }
 
     private fun showUserName() {
         //@TODO 4- depois de persistir o usuario exibir sempre as informacoes no EditText  se a sharedpref possuir algum valor, exibir no proprio editText o valor salvo
+        val sharedPreferences = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
+        val valorSalvo = sharedPreferences.getString("userKey", "")
+        nomeUsuario.setText(valorSalvo)
+
+        if (valorSalvo != null) {
+            if (valorSalvo.isNotEmpty()) {
+                Toast.makeText(this, "Valor salvo carregado: $valorSalvo", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                Toast.makeText(this, "Nenhum usuário encontrado encontrado.", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
     }
 
     //Metodo responsavel por fazer a configuracao base do Retrofit
@@ -55,11 +94,15 @@ class MainActivity : AppCompatActivity() {
            URL_BASE da API do  GitHub= https://api.github.com/
            lembre-se de utilizar o GsonConverterFactory mostrado no curso
         */
-    }
 
-    //Metodo responsavel por buscar todos os repositorios do usuario fornecido
-    fun getAllReposByUserName() {
-        // TODO 6 - realizar a implementacao do callback do retrofit e chamar o metodo setupAdapter se retornar os dados com sucesso
+        if (retrofit == null) {
+            val baseUrl = "https://api.github.com/"
+
+            retrofit = Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
     }
 
     // Metodo responsavel por realizar a configuracao do adapter
@@ -68,33 +111,71 @@ class MainActivity : AppCompatActivity() {
             @TODO 7 - Implementar a configuracao do Adapter , construir o adapter e instancia-lo
             passando a listagem dos repositorios
          */
+        val recyclerView = findViewById<RecyclerView>(R.id.rv_lista_repositories) // Substitua pelo ID correto
+        val adapter = RepositoryAdapter(list)
+        recyclerView.adapter = adapter
+
     }
 
+    //Metodo responsavel por buscar todos os repositorios do usuario fornecido
+    fun getAllReposByUserName() {
+        // TODO 6 - realizar a implementacao do callback do retrofit e chamar o metodo setupAdapter se retornar os dados com sucesso
+        val service = retrofit?.create(GitHubService::class.java)
+        val textoInserido = nomeUsuario.toString()
 
-    // Metodo responsavel por compartilhar o link do repositorio selecionado
-    // @Todo 11 - Colocar esse metodo no click do share item do adapter
-    fun shareRepositoryLink(urlRepository: String) {
-        val sendIntent: Intent = Intent().apply {
-            action = Intent.ACTION_SEND
-            putExtra(Intent.EXTRA_TEXT, urlRepository)
-            type = "text/plain"
+        service?.getAllRepositoriesByUser(textoInserido)?.enqueue(object :
+            Callback<List<Repository>> {
+            override fun onResponse(
+                call: Call<List<Repository>>,
+                response: Response<List<Repository>>
+            ) {
+                if (response.isSuccessful) {
+                    // Os dados foram recuperados com sucesso
+                    val repos = response.body()
+                    if (repos != null) {
+                        // Chame o método setupAdapter para processar os dados
+                        setupAdapter(repos)
+                    } else {
+                        Log.d("erro 1","Trate o caso em que a resposta não possui dados")
+                    }
+                } else {
+                    Log.d("erro 2", "Trate o caso em que a resposta não foi bem-sucedida (por exemplo, erro 404)")
+                }
+            }
+
+            override fun onFailure(call: Call<List<Repository>>, t: Throwable) {
+                Log.d("erro 3","Trate o caso em que a chamada falhou (por exemplo, problemas de rede)")
+            }
+        })
+
+        // Metodo responsavel por compartilhar o link do repositorio selecionado
+        // @Todo 11 - Colocar esse metodo no click do share item do adapter
+        fun shareRepositoryLink(urlRepository: String) {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, urlRepository)
+                type = "text/plain"
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
         }
 
-        val shareIntent = Intent.createChooser(sendIntent, null)
-        startActivity(shareIntent)
-    }
+        // Metodo responsavel por abrir o browser com o link informado do repositorio
 
-    // Metodo responsavel por abrir o browser com o link informado do repositorio
-
-    // @Todo 12 - Colocar esse metodo no click item do adapter
-    fun openBrowser(urlRepository: String) {
-        startActivity(
-            Intent(
-                Intent.ACTION_VIEW,
-                Uri.parse(urlRepository)
+        // @Todo 12 - Colocar esse metodo no click item do adapter
+        fun openBrowser(urlRepository: String) {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse(urlRepository)
+                )
             )
-        )
+
+
+        }
 
     }
 
 }
+
--- app/src/main/java/br/com/igorbag/githubsearch/ui/adapter/RepositoryAdapter.kt
+++ app/src/main/java/br/com/igorbag/githubsearch/ui/adapter/RepositoryAdapter.kt
@@ -1,8 +1,12 @@
 package br.com.igorbag.githubsearch.ui.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import br.com.igorbag.githubsearch.R
 import br.com.igorbag.githubsearch.domain.Repository
@@ -10,36 +14,8 @@ import br.com.igorbag.githubsearch.domain.Repository
 class RepositoryAdapter(private val repositories: List<Repository>) :
     RecyclerView.Adapter<RepositoryAdapter.ViewHolder>() {
 
-    var carItemLister: (Repository) -> Unit = {}
     var btnShareLister: (Repository) -> Unit = {}
 
-    // Cria uma nova view
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.repository_item, parent, false)
-        return ViewHolder(view)
-    }
-
-    // Pega o conteudo da view e troca pela informacao de item de uma lista
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        //@TODO 8 -  Realizar o bind do viewHolder
-        //Exemplo de Bind
-        //  holder.preco.text = repositories[position].atributo
-
-        // Exemplo de click no item
-        //holder.itemView.setOnClickListener {
-        // carItemLister(repositores[position])
-        //}
-
-        // Exemplo de click no btn Share
-        //holder.favorito.setOnClickListener {
-        //    btnShareLister(repositores[position])
-        //}
-    }
-
-    // Pega a quantidade de repositorios da lista
-    //@TODO 9 - realizar a contagem da lista
-    override fun getItemCount(): Int = 0
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         //@TODO 10 - Implementar o ViewHolder para os repositorios
@@ -51,7 +27,54 @@ class RepositoryAdapter(private val repositories: List<Repository>) :
         //        atributo = findViewById(R.id.item_view)
         //    }
 
+        var nameTextView: TextView = itemView.findViewById(R.id.tv_repo_name)
+        var shareButton: Button = itemView.findViewById(R.id.iv_share)
     }
-}
+
+
+        // Cria uma nova view
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view =
+                LayoutInflater.from(parent.context).inflate(R.layout.repository_item, parent, false)
+            return ViewHolder(view)
+        }
+
+
+        // Pega o conteudo da view e troca pela informacao de item de uma lista
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            //@TODO 8 -  Realizar o bind do viewHolder
+            //Exemplo de Bind
+            //  holder.preco.text = repositories[position].atributo
+
+            // Exemplo de click no item
+            //holder.itemView.setOnClickListener {
+            // carItemLister(repositores[position])
+            //}
+
+            // Exemplo de click no btn Share
+            //holder.favorito.setOnClickListener {
+            //    btnShareLister(repositores[position])
+            //}
+
+
+
+
+            val repo = repositories[position]
+            holder.nameTextView.text = repo.name
+            holder.shareButton.setOnClickListener {
+                btnShareLister(repositories[position])
+
+            }
+        }
+
+        // Pega a quantidade de repositorios da lista
+        //@TODO 9 - realizar a contagem da lista
+        override fun getItemCount(): Int {
+            return repositories.size
+        }
+
+
+    }
+
 
 
